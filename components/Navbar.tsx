92071bfcff557aa5aa55d154f0c06159e318b33b
tognia <h.tognia@ziyouma-agency.com>
2024-07-05 14:53:32 +0100

    Start Building Header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {logo} from "../public/assets/images/index";
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div>
-      NAVBAR
+    <div className='w-full bg-blue text-white'>
+      <div className='max-w-Container mx-auto h-20 px-4 flex items-center justify-between gap-2'>
+
+        <div className='h-12 px-5 rounded-full bg-transparent flex items-center 
+                      gap-2 hover:bg-hoverBg duration-300 cursor-pointer'>
+          <Image src={logo} className='w-44' alt='Logo' />
+        </div>
+
+      </div>
     </div>
   )
 } 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Container } from "postcss";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -13,7 +14,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens:{
+        xs:"320px",
+        sm:"375px",
+        sml:"500px",
+        md:"667px",
+        mdl:"768px",
+        lg:"960px",
+        lgl:"1024px",
+        xl:"1280px",
+        "2xl":"1400px"
+      },
+      colors:{
+        blue:"#0071dc",
+        lightBlue:"#e6f1fc",
+        yellow:"#ffc220",
+        hoverBg:"#004f9a",
+        lightText:"#46474a",
+      },
+      boxShadow: {
+        bannerShadow : "0 1px 2px 1px #00000026",
+      }
     },
+    maxWidth:{
+      Container:"1520px",
+      contentContainer:"1280px"
+    }
   },
   plugins: [],
 };
