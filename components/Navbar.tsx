bf08e81f558060fac51d86f6706051a5f0e8cd47
tognia <h.tognia@ziyouma-agency.com>
2024-07-05 15:22:24 +0100

    Start Building Header Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,20 @@ import Image from 'next/image';
 const Navbar = () => {
   return (
     <div className='w-full bg-blue text-white'>
-      <div className='max-w-Container mx-auto h-20 px-4 flex items-center justify-between gap-2'>
+      <div className='max-w-Container mx-auto h-20 px-4 flex items-center gap-2'>
 
-        <div className='h-12 px-5 rounded-full bg-transparent flex items-center 
-                      gap-2 hover:bg-hoverBg duration-300 cursor-pointer'>
+        <div className='navBarHover'>
           <Image src={logo} className='w-44' alt='Logo' />
+        </div>
+
+        <div className='navBarHover'>
+          <div className='w-4 grid grid-cols-2 gap-[2px]'>
+            <span className='w-1.5 h-1.5 border-[1px] border-white inline-flex'></span>
+            <span className='w-1.5 h-1.5 border-[1px] border-white inline-flex'></span>
+            <span className='w-1.5 h-1.5 border-[1px] border-white inline-flex'></span>
+            <span className='w-1.5 h-1.5 border-[1px] border-white inline-flex'></span>
+          </div>
+          <p>Departments</p>
         </div>
 
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import "../styles/global.css";
 import type { AppProps } from 'next/app';
+import { Open_Sans } from "next/font/google";
+
+const open_sans = Open_Sans ({
+    subsets: ["latin"],
+    variable:"--font_open-sans",
+})
 
 export default function App({Component, pageProps}:AppProps) {
     return <Component {...pageProps} />
