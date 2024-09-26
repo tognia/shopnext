efc7c69fe62d238ff63d27c4a29da10b3cfe3374
tognia <h.tognia@ziyouma-agency.com>
2024-09-26 09:30:35 +0100

    Works on CartPage

--- components/CartPage.tsx
+++ components/CartPage.tsx
@@ -13,13 +13,14 @@ import { HiMinusSmall } from "react-icons/hi2";
 import { MdOutlineAdd } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import Image from "next/image";
+import { StoreProduct } from "@/type";
 
 const CartPage = () => {
   const productData = useSelector((state: any) => state.shopper.productData);
   return (
     <div className="w-full py-10 bg-white">
       <div className="w-full flex gap-10">
-        <div className="w-2/3 flex flex-col gap-5 ">
+        <div className="w-2/3 flex flex-col  gap-5 ">
           <h1 className="text-2xl font-bold text-black">
             Cart
             <span className="text-lightText font-normal">
@@ -75,6 +76,44 @@ const CartPage = () => {
                 <button className="px-2 py-[1px] text-red-600 text-sm border-[1px] border-red-600 rounded-sm ">
                   Rollback
                 </button>
+              </div>
+              {/* ITEMS */}
+              <div>
+                {
+                  productData.map((item:StoreProduct)=>(
+                    <div key={item._id} className="flex items-center justify-between gap-4 border-b-[1px]
+                    border-b-zinc-200 pb-4">
+                        <div className="w-3/4 flex items-center gap-2">
+                        <Image
+                            className="w-32"
+                            width={500}
+                            height={500}
+                            src={item.image}
+                            alt="product img"
+                          />
+                        </div>
+                        <div>
+                          <h2 className="text-base text-zinc-500">{item.title}</h2>
+                          <p className="text-sm text-zinc-500">{item.description} </p>
+                          <p className="text-sm text-zinc-500">price: ${item.price} </p>
+                          <p className="text-sm text-zinc-500 flex items-center gap-1">
+                            <span className="bg-blue rounded-full text-white text-xs w-4 h-5
+                            flex items-center justify-center">
+                              <TbReload className="rotate-180" />
+                            </span>
+                            Free 30-day returns
+                          </p>
+                        </div>
+                        {/* Buttons */}
+                        <div>
+                          <button className="text-sm underline underline-offset-2 decoration-[1px] text-zinc-600
+                          hover:no-underline hover:text-blue  duration-300">
+                            Remove
+                          </button>
+                        </div>
+                    </div>
+                  ))
+                }
               </div>
             </div>
           </div>
