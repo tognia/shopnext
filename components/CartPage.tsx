e25ac1862d652e7d014d7e6ae7ffbf5a927f99ec
tognia <h.tognia@ziyouma-agency.com>
2025-01-02 11:00:28 +0100

    Add Some Improvments updtd

--- components/CartPage.tsx
+++ components/CartPage.tsx
@@ -264,8 +264,23 @@ const CartPage = () => {
             </div>
           </div>
 
-          <div>
-            
+          <div className="w-full flex flex-col gap-4 border-b-[1px] border-b-zinc-200 pb-4">
+            <div className="flex flex-col gap-1" >
+              <div className="text-sm flex justify-between">
+                  <p>Shipping</p>
+                  <p className="text-[#2a8703]">Free</p>
+              </div>
+              <div className="text-sm flex justify-between">
+                  <p className="font-semibold">Taxes</p>
+                  <p className="text-zinc-800">Calculated at checkout</p>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <p>Estimated Total</p>
+              <p className="text-zinc-800 font-bold text-lg">
+                    <FormatePrice amount={totalAmt} />
+                </p>
+            </div>
           </div>
 
         </div>
